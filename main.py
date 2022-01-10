04debb44b973804eb7e0dacd1edfccdb9ed4d114
Alex222345 <90255744+aAlexXs@users.noreply.github.com>
2022-01-10 13:32:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests 
+from bs4 import BeautifulSoup
+lapa = requests.get('http://vilanuvidusskola.blogspot.com/')
+print(lapa)
+print(lapa.content)
+soup = BeautifulSoup(lapa.content, 'html.parser')
